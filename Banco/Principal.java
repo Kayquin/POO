aa18b01de7adbc527f905ed313aee31fe1134846
kayquin <104329791+Kayquin@users.noreply.github.com>
2024-03-26 03:15:54 -0300

    Update Principal.java

--- Banco/Principal.java
+++ Banco/Principal.java
@@ -1,16 +1,18 @@
 package Principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
         Clientes(args);
         Agencia(args);
-        Conta(args);
         Investimentos(args);
+        Conta(args);
     }
 
     // Teste de função do método "Clientes"
-    public static void Clientes(String[] args) {
+    private static void Clientes(String[] args) {
 
         Clientes c1 = new Clientes();
         c1.setNome("João");
@@ -28,7 +30,7 @@ public class Principal {
     }
 
     // Teste de função do método "Agencia"
-    public static void Agencia(String[] args) {
+    private static void Agencia(String[] args) {
 
         Agencia a1 = new Agencia();
         a1.setCodigo(1);
@@ -46,34 +48,43 @@ public class Principal {
     }
 
     // Teste de função do método "Conta"
-    public static void Conta(String[] args) {
+    private static void Conta(String[] args) {
 
         Conta co1 = new Conta();
         co1.setNumero(1);
-        co1.setSaldo(1000);
+        co1.setSaldo(1000); //Valor inicial do Saldo
         System.out.println("\nDados da Conta ");
         System.out.println("Numero: " + co1.getNumero());
         System.out.println("Saldo: " + co1.getSaldo());
-
+        // Método sacar
+        System.out.println("Digite o valor a ser sacado: ");
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+        if(num < 0){ //Verifica se o valor é negativo, caso positivo realiza o saque
+            System.out.println("Valor inválido para saque.");
+        }else{
+            co1.sacar(num);
+        }
+        // Método depositar
+        System.out.println("Digite o valor a ser depositado: ");
+        Scanner sd = new Scanner(System.in);
+        double num2 = sd.nextDouble();
+        if (num2 < 0) { //Verifica se o valor é negativo, caso positivo realiza o deposito
+            System.out.println("Valor inválido para depósito.");
+        } else {
+            co1.depositar(num2);
+        }
     }
 
     // Teste de função do método "Investimentos"
-    public static void Investimentos(String[] args) {
+    private static void Investimentos(String[] args) {
 
-        Investimentos i1 = new Investimentos();
-        i1.setAgencia("1");
-        i1.setCliente("João");
-        i1.setConta("1");
-        i1.setGerente("Gerente 1");
-        i1.setInvestimento(1000000);
-        i1.setSaldo(0);
-        System.out.println("\nDados do Investimento ");
-        System.out.println("Agencia: " + i1.getAgencia());
-        System.out.println("Cliente: " + i1.getCliente());
-        System.out.println("Conta: " + i1.getConta());
-        System.out.println("Gerente: " + i1.getGerente());
-        System.out.println("Investimento: " + i1.getInvestimento());
-        System.out.println("Saldo: " + i1.getSaldo());
+        Investimentos inv1 = new Investimentos("0123", "João Silva", "123456", "Gerente 1", 1000, 100000);
+        inv1.imprimir();
+        inv1.imposto_investimento();
+        inv1.verifica_saldo();
 
-    }
+
+        
+    }   
 }
