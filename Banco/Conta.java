dba47398aaa284f54904271fbea3c82a4c11b7ed
kayquin <104329791+Kayquin@users.noreply.github.com>
2024-03-26 03:17:47 -0300

    Update Conta.java

--- Banco/Conta.java
+++ Banco/Conta.java
@@ -22,5 +22,26 @@ public class Conta {
         this.saldo = saldo;
     }
 
+    // Método sacar
+    public void sacar(double valor) {
+        if (valor > this.getSaldo()) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            double newSaldo = this.getSaldo() - valor;
+            this.setSaldo(newSaldo);
+            System.out.println("Novo saldo: " + this.getSaldo());
+        }
+    }
+
+    // Método depositar
+    public void depositar(double valor) {
+        if (valor < 0) {
+            System.out.println("Valor inválido para depósito.");
+            return;
+        }
+        this.saldo += valor;
+        System.out.println("Depósito realizado com sucesso!");
+        System.out.println("Novo saldo: " + this.getSaldo());
+    }
 
 }
